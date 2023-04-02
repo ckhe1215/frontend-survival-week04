2665cb4a831c19d1f3d84c555fe1247570079fdd
haeun kim <ckhe1215@gmail.com>
2023-04-03 00:52:11 +0900

    add cors setting

--- express-app/app.ts
+++ express-app/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const port = 3000;
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/restaurants', (req, res) => {
   const restaurants = [
